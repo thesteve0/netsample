f57e5bdf5b298667b7a8f0a196c901fff2efc628
thesteve0 <spousty@redhat.com>
2017-03-06 17:03:24 -0800

    Trying to make it simpler

--- Program.cs
+++ Program.cs
@@ -1,10 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
@@ -17,21 +11,15 @@ namespace WebCore.netsample
     {
         public static void Main(string[] args)
         {
-          var config = new ConfigurationBuilder()
-                .AddEnvironmentVariables("")
-            .Build();
-
             var url = config["ASPNETCORE_URLS"] ?? "http://*:8080";
 
             var host = new WebHostBuilder()
                 .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .UseUrls(url)
-                .Build();
-
+                .UseStartup<Program>()
+                .Build()
+                .UserUrls(url);
             host.Run();
+
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
