2405817b3e0dcaff75315ea10ceec40a3cfbbb02
thesteve0 <spousty@redhat.com>
2017-03-06 17:00:20 -0800

    Beware copy and paste

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+
 
 
 namespace WebCore.netsample
