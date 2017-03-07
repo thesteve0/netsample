c4359ea4f9b476134f7df604cc4c9727c4d7d778
thesteve0 <spousty@redhat.com>
2017-03-06 16:58:52 -0800

    Forgot the dependencies

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Builder;
-
+using Microsoft.Extensions.Configuration;
 
 
 namespace WebCore.netsample
